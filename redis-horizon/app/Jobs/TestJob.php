88d5547e2742209a08d2e88ce741629bb2f977a9
Rene Zehl <rene.zehl@gmx.de>
2026-04-26 20:56:27 +0200

    feat(redis-horizon): add TestJob, OrderShipped mailable, and queue demo routes

---
+++ redis-horizon/app/Jobs/TestJob.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
+
+class TestJob implements ShouldQueue
+{
+    use Queueable;
+
+    protected User $user;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        sleep(2);
+        Log::info('Job completed for User ID: ' . $this->user->id);
+    }
+}
--- redis-horizon/routes/web.php
+++ redis-horizon/routes/web.php
@@ -1,7 +1,36 @@
 <?php
 
+use App\Mail\OrderShipped;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+use App\Models\User;
+use App\Jobs\TestJob;
+
 
 Route::get('/', function () {
+    // Redis::set('name', 'rene');
+    // $name = Redis::get('name');
+
+    // dd($name);
+
+
     return view('welcome');
+});
+
+Route::post('/', function () {
+    Mail::to('test@test.com')->queue(new OrderShipped);
+
+    return redirect('/');
+});
+
+
+Route::get('/jobs/{jobs}/{user}', function ($jobs, $user) {
+    $user = User::find($user);
+
+    for ($i = 0; $i <= $jobs; $i++) {
+        TestJob::dispatch($user);
+    }
+
+    return redirect('/');
 });
