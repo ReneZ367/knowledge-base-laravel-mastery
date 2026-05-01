baf02d1ec2148cd4946557aa5d8b0a006d91287c
Rene Zehl <rene.zehl@gmx.de>
2026-05-01 11:24:04 +0200

    feat(filament-livewire): configure Filament panel, auth, and branding

--- filament-livewire/app/Providers/Filament/SecretAdminPanelProvider.php
+++ filament-livewire/app/Providers/Filament/SecretAdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\EditProfile;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -27,9 +28,21 @@ class SecretAdminPanelProvider extends PanelProvider
             ->default()
             ->id('secret-admin')
             ->path('secret-admin')
+            ->unsavedChangesAlerts()
+            ->databaseTransactions()
+            // ->registration()
+            ->passwordReset()
+            ->profile(EditProfile::class)
+            ->emailVerification()
+            ->emailChangeVerification()
             ->login()
+            ->brandLogo(asset('images/logo_dark.png'))
+            ->darkModeBrandLogo(asset('images/logo_white.png'))
+            ->brandLogoHeight('2.5rem')
             ->colors([
-                'primary' => Color::Amber,
+                // 'primary' => Color::Amber,
+                'primary' => Color::convertToOklch('#4bb893'),
+                'success' => Color::convertToOklch('#80de5a'),
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
